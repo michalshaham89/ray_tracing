19301b0dc74c0fd15fa086ad27461cc7b987ff1c
michalshaham89 <michalshaham@mail.tau.ac.il>
2020-05-27 16:04:58 +0300

    barotropic

--- ray_tracing.py
+++ ray_tracing.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy import integrate
+from matplotlib import pyplot as plt
 
 
 class BarotropicBackFlow(object):
@@ -12,29 +13,24 @@ class BarotropicBackFlow(object):
         self.N = 0.01
 
     def f_eff(self, x):
-        return self.f + 0.5 * self.dVx_dx(x)
+        return self.f + 0.5 * self.dV_dx(x)
 
     @staticmethod
-    def Vx(x):
+    def V(x):
         return -0.2 * np.exp(-1e-8 * x ** 2)
 
     @staticmethod
-    def dVx_dx(x):
+    def dV_dx(x):
         return 4e-9 * x * np.exp(-1e-8 * x ** 2)
 
     @staticmethod
-    def d2Vx_dx2(x):
+    def d2V_dx2(x):
         return (-8e-17 * x ** 2 + 4e-9) * np.exp(-1e-8 * x ** 2)
 
 
 class Ray(object):
     """
-    contains data about the ray.
-
-    I am not sure if to separate variables and function to x, y, z,
-    or to leave it in a 'vector' mode. ( k=[k_x, k_y, k_z] )
-    it will be easier to switch to 2d when it is in vector mode,
-    but the code will be less 'readable'.
+    contains ray attributes.
     """
 
     def __init__(self, option):
@@ -42,97 +38,129 @@ class Ray(object):
             self.BF = BarotropicBackFlow()
             self.Barotropic = True
 
-    def calc_Cg_x(self, x, kx, kz):
+    def Cg_x(self, x, kx, kz):
         if self.Barotropic:
-            return self.BF.N ** 2 * kx / self.BF.f_eff(x) / kz ** 2
+            return self.BF.N ** 2 * kx / self.BF.f / kz ** 2
         else:
-            return self.BF.N ** 2 * kx / self.BF.f_eff(x) / kz ** 2
+            return self.BF.N ** 2 * kx / self.BF.f / kz ** 2
 
-    def calc_Cg_z(self, x, kx, kz):
+    def Cg_z(self, x, kx, kz):
+        """
+        why is sign(Cg_z)=-sign(kz)??
+        """
         if self.Barotropic:
-            return -self.BF.N ** 2 * kx ** 2 / self.BF.f_eff(x) / kz ** 3
+            return -self.BF.N ** 2 * kx ** 2 / self.BF.f / kz ** 3
         else:
-            return -self.BF.N ** 2 * kx ** 2 / self.BF.f_eff(x) / kz ** 3 + kx / kz ** 2 * self.BF.dVx_dz(x)
-
-    def ode_x(self, x, kx, kz):
-        if self.Barotropic:
-            return self.BF.Vx(x) + self.calc_Cg_x(x, kx, kz)
-        else:
-            return self.BF.Vx(x) + self.calc_Cg_x(x, kx, kz)
-
-    def ode_z(self, x, kx, kz):
-        if self.Barotropic:
-            return self.calc_Cg_z(x, kx, kz)
-        else:
-            return self.calc_Cg_z(x, kx, kz)
+            return -self.BF.N ** 2 * kx ** 2 / self.BF.f / kz ** 3 + kx / kz ** 2 * self.BF.dV_dz(x)
 
     def ode_kx(self, x, kx, kz):
         if self.Barotropic:
-            return -0.5 * self.BF.d2Vx_dx2(x) - kx * self.BF.dVx_dx(x)  # only if doppler shift k*V is relevant
+            return -0.5 * self.BF.d2V_dx2(x)
         else:
-            return -0.5 * self.BF.d2Vx_dx2(x) \
-                   + kx / kz * self.BF.d2Vx_dxdz(x) \
-                   - kx ** 2 / self.BF.f(x) / kz ** 2 * self.BF.N(x) * self.BF.dN_dx(x) \
-                   - kx * self.BF.dVx_dx(x)  # only if doppler shift k*V is relevant
+            return -0.5 * self.BF.d2V_dx2(x) \
+                   + kx / kz * self.BF.d2V_dxdz(x) \
+                   - kx ** 2 / self.BF.f / kz ** 2 * self.BF.N(x) * self.BF.dN_dx(x) \
+                   - kx * self.BF.dV_dx(x)  # only if doppler shift k*V is relevant
 
     def ode_kz(self, x, kx, kz):
         if self.Barotropic:
             return 0
         else:
-            return -0.5 * self.BF.d2Vx_dxdz(x) \
-                   + kx / kz * self.BF.d2Vx_dz2(x) \
+            return -0.5 * self.BF.d2V_dxdz(x) \
+                   + kx / kz * self.BF.d2V_dz2(x) \
                    - kx ** 2 / self.BF.f / kz ** 2 * self.BF.N(x) * self.BF.dN_dz(x) \
-                   - kz * self.BF.dVx_dz(x)  # only if doppler shift k*V is relevant
+                   - kz * self.BF.dV_dz(x)  # only if doppler shift k*V is relevant
+
+    def omega(self, x, kx, kz):
+        # make sure it is constant
+        self.BF.f_eff(x) + self.BF.N ** 2 * kx ** 2 / 2 / self.BF.f / kz ** 2
 
 
 class RayTracing(Ray):
     # simulates a coupled pendulums system, including pendulums and springs
-    def __init__(self, x0, z0, kx0, kz0, t0=0, t_final=1, N_eval=11, option=1):
-        # initialize pendulums system data. accepts input file name or path (string)
-        # initialize ray data.
+    def __init__(self, t0=0, t_final=24 * 30 * 12, N_eval=201, option=1, file_name='Data.out'):
         Ray.__init__(self, option=option)
-        self.x0, self.z0, self.kx0, self.kz0 = x0, z0, kx0, kz0
-        self.t0 = t0
-        self.t_final = t_final
-        self.t_eval = np.linspace(t0, t_final, N_eval)
+        self.file_name = file_name
 
-    def progressing_eq(self, t, var0):
+        self.t0 = t0 * 3600
+        self.t_final = t_final * 3600
+        self.t_eval = np.linspace(self.t0, self.t_final, N_eval)
+
+    def differential_eq(self, t, var0):
         """
-        this equation returns differential equations, Runge-Kutta use.
+        this equation returns differential equations
         """
         if self.Barotropic:
-            return self.ode_x(var0[0], var0[2], var0[3]), self.ode_z(var0[0], var0[2], var0[3]), \
+            return self.Cg_x(var0[0], var0[2], var0[3]), self.Cg_z(var0[0], var0[2], var0[3]), \
                    self.ode_kx(var0[0], var0[2], var0[3]), self.ode_kz(var0[0], var0[2], var0[3])
         else:
-            return self.ode_x(*var0), self.ode_z(*var0), self.ode_kx(*var0), self.ode_kz(*var0)
+            return self.Cg_x(*var0), self.Cg_z(*var0), self.ode_kx(*var0), self.ode_kz(*var0)
 
-    def output_line(self, t, x, z, kx, kz):
-        """
-        write data to output file
-        """
-        output_line = '\n{0:6.6f}\t{1:6.6f}  {2:6.6f}\t{3:6.6f}  {4:6.6f}'.format(t, x, z, kx, kz)
+    def output_line(self, t, x, z, kx, kz, omega):
+        output_line = '{0:6.6f}\t{1:6.6f}\t{2:6.6f}\t{3:6.6f}\t{4:6.6f}\n'.format(t, x, z, kx, kz, omega)
         return output_line
 
     def open_data_file(self):
-        file_output = open('Data.out', 'w')
-        file_output.write('\ntime\tx\tz\tk_x\tk_z')
+        file_output = open(self.file_name, 'w')
+        file_output.write('time\tx\tz\tk_x\tk_z\tomega\n')
         return file_output
 
-    def main(self):
+    def main(self, x0, z0, lx0, lz0, x_lim=[-30e3, 34e3], z_lim=-0.5e3):
         """
-        the main function runs the algorithm in a loop.
-        it writes data to the output file N times.
+        uses integrate.solve_ivp, with method='RK45' to solve the differential equations.
         """
-        # open output file
+        kx0, kz0 = 2 * np.pi / lx0, 2 * np.pi / lz0
 
-        sol = integrate.solve_ivp(fun=self.progressing_eq, t_span=(self.t0, self.t_final),
-                                  y0=(self.x0, self.z0, self.kx0, self.kz0), method='RK45', t_eval=self.t_eval)
+        def z_wall(t, y): return y[1] - z_lim
+        z_wall.terminal = True
+        def x_wall1(t, y): return y[0] - x_lim[0]
+        x_wall1.terminal = True
+        def x_wall2(t, y): return y[0] - x_lim[1]
+        x_wall2.terminal = True
 
+        sol = integrate.solve_ivp(fun=self.differential_eq, t_span=(self.t0, self.t_final), y0=(x0, z0, kx0, kz0),
+                                  method='RK45', t_eval=self.t_eval, events=[x_wall1, x_wall2, z_wall])
+
+        # write to output file
         file_output = self.open_data_file()
-        for i in range(len(self.t_eval)):
-            file_output.write(self.output_line(sol.t[i], *sol.y[:, i]))
+        for i in range(len(sol.t)):
+            x, z, kx, kz = sol.y[:, i]
+            file_output.write(self.output_line(sol.t[i], x, z, kx, kz, self.omega(x, kx, kz)))
         file_output.close()
 
 
-ray_system = RayTracing(0, 0, 1, 1)
-ray_system.main()
+def plot_simulation(file_name):
+    """
+    plots data from file
+    """
+    with open(file_name, 'r') as f:
+        next(iter(f))
+        x = []
+        z = []
+        for line in f:
+            line = line.split()
+            x.append(float(line[1]) / 1000)
+            z.append(float(line[2]) / 1000)
+
+    plt.plot(x, z)
+
+
+if __name__ == "__main__":
+    ray_system = RayTracing()
+
+    for x0, z0 in [(-20e3, 0), (-10e3, 0), (0, 0), (5e3, 0), (10e3, 0)]:
+        ray_system.main(x0, z0, 40e3, 1e2)
+        plot_simulation('Data.out')
+
+    # plot background flow
+    x = np.linspace(-12, 12, 50)
+    z = np.linspace(-0.5, 0, 50)
+    X, Z = np.meshgrid(x, z)
+    V = ray_system.BF.V(X / 1000)
+    plt.contour(X, Z, V, 4, colors='black')
+
+    plt.title('ray tracing')
+    plt.ylabel('z [km]')
+    plt.xlabel('x [km]')
+    plt.grid()
+    plt.show()
